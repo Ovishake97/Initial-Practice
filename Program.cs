da141bfb3dab9cccba1d419a1b92cc22919c293d
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-09-25 13:56:39 +0530

    File added

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is to print Hello World 
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
